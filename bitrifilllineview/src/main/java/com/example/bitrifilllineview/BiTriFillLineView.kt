c8353588fffc122101a0f9b6d57cf8bb624e03db
Anwesh43 <anweshthecool0@gmail.com>
2020-11-01 01:59:27 +0530

    adding BTFLNode class

--- bitrifilllineview/src/main/java/com/example/bitrifilllineview/BiTriFillLineView.kt
+++ bitrifilllineview/src/main/java/com/example/bitrifilllineview/BiTriFillLineView.kt
@@ -136,4 +136,45 @@ class BiTriFillLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BTFLNode(var i : Int, val state : State = State()) {
+
+        private var next : BTFLNode? = null
+        private var prev : BTFLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BTFLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBTFLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BTFLNode {
+            var curr : BTFLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
