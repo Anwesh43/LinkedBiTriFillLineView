2eb4b27528ca10ecbfc389732b73f261af453096
Anwesh43 <anweshthecool0@gmail.com>
2020-11-01 02:07:03 +0530

    adding Renderer class

--- bitrifilllineview/src/main/java/com/example/bitrifilllineview/BiTriFillLineView.kt
+++ bitrifilllineview/src/main/java/com/example/bitrifilllineview/BiTriFillLineView.kt
@@ -200,4 +200,27 @@ class BiTriFillLineView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : BiTriFillLineView) {
+
+        private val animator : Animator = Animator(view)
+        private val btfl : BiTriFillLine = BiTriFillLine(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            btfl.draw(canvas, paint)
+            animator.animate {
+                btfl.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            btfl.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
