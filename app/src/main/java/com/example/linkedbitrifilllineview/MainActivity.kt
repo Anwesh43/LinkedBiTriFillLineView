9244307c5aadd26bb027db0b625e440c569c8ed7
Anwesh43 <anweshthecool0@gmail.com>
2020-11-01 02:15:19 +0530

    adding fullscreen and create view

--- app/src/main/java/com/example/linkedbitrifilllineview/MainActivity.kt
+++ app/src/main/java/com/example/linkedbitrifilllineview/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.linkedbitrifilllineview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.bitrifilllineview.BiTriFillLineView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BiTriFillLineView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
