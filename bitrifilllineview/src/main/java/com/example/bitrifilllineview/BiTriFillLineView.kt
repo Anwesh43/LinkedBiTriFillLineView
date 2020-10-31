8c6cdc739f48d9b1269b49467ff3aa4c8ea049b8
Anwesh43 <anweshthecool0@gmail.com>
2020-11-01 01:33:26 +0530

    adding constants and libraries

--- bitrifilllineview/src/main/java/com/example/bitrifilllineview/BiTriFillLineView.kt
+++ bitrifilllineview/src/main/java/com/example/bitrifilllineview/BiTriFillLineView.kt
@@ -1,2 +1,28 @@
 package com.example.bitrifilllineview
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<Int> = arrayOf(
+    "#F44336",
+    "#673AB7",
+    "#FF9800",
+    "#3F51B5",
+    "#4CAF50"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 3.2f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val scGap : Float = 0.02f / strokeFactor
+val rot : Float = 180f
+
