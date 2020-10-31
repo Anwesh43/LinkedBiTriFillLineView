9384baf9c369daa0b9e30d0a7a463dde6d279a3b
Anwesh43 <anweshthecool0@gmail.com>
2020-11-01 01:48:12 +0530

    adding BiTriFillLineView class

--- bitrifilllineview/src/main/java/com/example/bitrifilllineview/BiTriFillLineView.kt
+++ bitrifilllineview/src/main/java/com/example/bitrifilllineview/BiTriFillLineView.kt
@@ -73,3 +73,19 @@ fun Canvas.drawBTFLNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBiTriFillLine(scale, w, h, paint)
 }
+
+class BiTriFillLineView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
